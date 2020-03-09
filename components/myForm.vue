45e57f02746a1d86e1a1dfd70f0d0887daa57687
Benjamin Russell <benjamin.russell@hoglundlaw.hoglunddomain>
2020-03-09 14:52:25 -0500

    :(

--- components/myForm.vue
+++ components/myForm.vue
@@ -41,7 +41,7 @@
             </label>
 
             <label id="messageLabel" for="message">
-              <span aria-label="Message" aria-hidden="true"></span>
+              <span aria-label="Message" aria-hidden="true">:(</span>
             <textarea class="slot-2-1-1" id="message" name="message" placeholder="* Message" value="" ></textarea>
             </label>
 
